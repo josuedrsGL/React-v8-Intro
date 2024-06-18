e48a25927010f9583546a6d65881d31d28435700
Josue Rodriguez <josue.rodriguez.siatama@gorillalogic.com>
2024-06-17 22:26:22 -0500

    adding new dependencies: eslint, prettier

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h1", {}, props.animal),
-        React.createElement("h1", {}, props.breed),
-    ]);
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h1", {}, props.animal),
+    React.createElement("h1", {}, props.breed),
+  ]);
+};
 
 //declaration of component
-const App = () =>{
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Do you like it?, Adopt me!"),
-            React.createElement(Pet,{
-                name: "honey",
-                animal: "dog",
-                breed: "german sheperd"
-            }),
-            React.createElement(Pet,{
-                name: "lulú",
-                animal: "dog",
-                breed: "French puddle"
-            }),
-            React.createElement(Pet,{
-                name: "Temari",
-                animal: "cat",
-                breed: "Azul ruso"
-            }),
-        ]
-    )
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Do you like it?, Adopt me!"),
+    React.createElement(Pet, {
+      name: "honey",
+      animal: "dog",
+      breed: "german sheperd",
+    }),
+    React.createElement(Pet, {
+      name: "lulú",
+      animal: "dog",
+      breed: "French puddle",
+    }),
+    React.createElement(Pet, {
+      name: "Temari",
+      animal: "cat",
+      breed: "Azul ruso",
+    }),
+  ]);
 };
 // Getting the root DOM context
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 // passing root context to react
 const root = ReactDOM.createRoot(container);
 // rendering the component
