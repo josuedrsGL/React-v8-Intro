e3cc95e89dd4723585ac190be5e655bee8f0e3c7
Josue Rodriguez <josue.rodriguez.siatama@gorillalogic.com>
2024-06-18 09:58:24 -0500

    modules and eslint for react

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,19 @@
-import React from "react";
 import {createRoot} from "react-dom/client";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h1", {}, props.animal),
-    React.createElement("h1", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 //declaration of component
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Do you like it?, Adopt me!"),
-    React.createElement(Pet, {
-      name: "honey",
-      animal: "dog",
-      breed: "german sheperd",
-    }),
-    React.createElement(Pet, {
-      name: "lulú",
-      animal: "dog",
-      breed: "French puddle",
-    }),
-    React.createElement(Pet, {
-      name: "Temari",
-      animal: "cat",
-      breed: "Azul ruso",
-    }),
-  ]);
+  <div>
+    <h1>Do you like it?, Adopt me!</h1>
+    <Pet name="honey" animal="dog" breed="german sheperd"/>
+    <Pet name="lulú" animal="dog" breed="French puddle"/>
+    <Pet name="Temari" animal="cat" breed="Azul ruso"/>
+  </div>
 };
+
 // Getting the root DOM context
 const container = document.getElementById("root");
 // passing root context to react
 const root = createRoot(container);
 // rendering the component
-root.render(React.createElement(App));
+root.render(<App />)
