0b6225ab91df2a081c6ec38e04ac72d4aa5f6a78
Josue Rodriguez <josue.rodriguez.siatama@gorillalogic.com>
2024-06-19 18:08:11 -0500

    react query implemented

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import { Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 //declaration of component
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Do you like it?, Adopt me!</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<SearchParams />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Do you like it?, Adopt me!</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,33 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
-  const { id } = useParams();
-  return <h2>hi! {id} </h2>;
+  const { id } = useParams(); //with this we can use the params passed in the url from router directly in my component
+  const results = useQuery(["details", id], fetchPet); //if you don't have id in details, make a fetch
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data;
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          weight: {pet.weight} Height: {pet.height} Order: {pet.order}
+        </h2>
+        <button>Adopt {pet.name}</button>
+        <p>Location area encounters: {pet.location_area_encounters}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
