ec753588989fba416f62208e6477fab7a155023b
Josue Rodriguez <josue.rodriguez.siatama@gorillalogic.com>
2024-06-20 11:10:04 -0500

    adding error generic boundary

---
+++ src/Demo.jsx
@@ -0,0 +1,13 @@
+const Demo = () => {
+  //   throwError();
+  return (
+    <div>
+      <h1> Simulate a component</h1>
+    </div>
+  );
+};
+export default Demo;
+
+function throwError() {
+  throw new Error("It was an error");
+}
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
+import ErrorBoundary from "./ErrorBoundary";
+import Demo from "./Demo";
 
 const Details = () => {
   const { id } = useParams(); //with this we can use the params passed in the url from router directly in my component
@@ -25,9 +27,20 @@ const Details = () => {
         </h2>
         <button>Adopt {pet.name}</button>
         <p>Location area encounters: {pet.location_area_encounters}</p>
+        <Demo />
       </div>
     </div>
   );
 };
+// export default Details;
 
-export default Details;
+// We have to implements in this way for cover the entire component "details"
+function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
+
+export default DetailsErrorBoundary;
